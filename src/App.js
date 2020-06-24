f93d143e47d0eaf47843884f11e60c214a258159
MohsinAliSoomro <mohsinalisoomro23@gmail.com>
2020-06-24 07:35:34 +0500

    CreateContext hook use of basic cunter increment app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Parent from './component/parent';
 import './App.css';
 import counterContext from './component/countContext';
 
 function App() {
+  let counterState=useState(0);
   return (
-    <counterContext.Provider value={224}>
+    <counterContext.Provider value={counterState}>
       <div className="App">
         <Parent name="Taha" />
       </div>
--- src/component/child.js
+++ src/component/child.js
@@ -1,12 +1,15 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useContext } from 'react';
 import CounterContext from './countContext'
-function Child(props){
-    let counterContext=useContext(CounterContext)
-    return(
+function Child(props) {
+    let counterContext = useContext(CounterContext)
+    return (
         <div>
             <h1>Child component </h1>
             <h1>Your Name is {props.name} </h1>
-    <h1>Value of counter is {counterContext}</h1>
+            <h1>Value of counter is {counterContext[0]}</h1>
+            <button onClick={()=>counterContext[1](++counterContext[0])}>
+                Increment
+            </button>
         </div>
     );
 }
